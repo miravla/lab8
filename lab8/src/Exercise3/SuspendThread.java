38931ce895a4f43ee8e0918484bec8635250f64c
Angel <qifang_2012@outlook.com>
2021-05-09 17:34:36 +0800

    Add Content

--- lab8/src/Exercise3/SuspendThread.java
+++ lab8/src/Exercise3/SuspendThread.java
@@ -1,5 +1,96 @@
 package Exercise3;
 
-public class SuspendThread {
+
+import java.util.LinkedHashSet;
+import java.util.Random;
+import java.util.Set;
+
+public class SuspendThread implements Runnable {
+
+	//set word to arrange
+	private String randomArrange[] = { "it", "is", "recommended", "to", "use", "Calendar", "class" };
+	
+	
+	private static boolean isRunning = false;
+
+	//rearrange word in random number generated
+	private void rearrangeSentence() {
+
+		Random random = new Random();
+		
+		Set<Integer> set = new LinkedHashSet<Integer>();
+
+		while (set.size() < randomArrange.length) {
+			
+			int randomNumber = random.nextInt(randomArrange.length);
+			set.add(randomNumber);
+		}
+
+		Object newSequence[] = set.toArray();
+
+		String newSentence = "";
+		
+		for (int counter = 0; counter < randomArrange.length; counter++) {
+			newSentence += randomArrange[(int) newSequence[counter]] + " ";
+			
+			//indicate thread is executing
+			isRunning = true;
+		}
+		System.out.println(newSentence);
+	}
+
+	//suspend thread
+	public void rearrangeSentence(String threadName) {
+		Random random = new Random();
+		Set<Integer> set = new LinkedHashSet<Integer>();
+
+		while (set.size() < randomArrange.length) {
+			int randomNum = random.nextInt(randomArrange.length);
+			set.add(randomNum);
+		}
+
+		Object newSequence[] = set.toArray();
+
+		String newSentence = threadName + " : ";
+		for (int j = 0; j < randomArrange.length; j++) {
+			newSentence += randomArrange[(int) newSequence[j]] + " ";
+			try {
+				//display message if thread is suspending
+				if (isRunning == true) {
+					System.out.println("\n\n" + threadName + " is suspending for 5 seconds" + "\n\n");
+					
+					//set timer to 5 s
+					Thread.sleep(5000);
+					isRunning = false;
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println(newSentence);
+	}
+
+
+	//take out the word 1 by 1 in text
+		private void extractString() {
+			String text = "Nezuko Is super Cute";
+			for(int j = 0; j < 20; j++) {
+				String subText = text.substring(0, j + 1);
+				System.out.println(subText);
+			}
+		}
+		
+	@Override
+	public void run() {
+		Thread currentThread = Thread.currentThread();
+		String threadName = currentThread.getName();
+		if (threadName.equals("text")) {
+			extractString();
+		} else if (threadName.equals("word1")) {
+			rearrangeSentence();
+		} else if (threadName.equals("suspendText")) {
+			rearrangeSentence(threadName);
+		}
+	}
 
 }
--- lab8/src/Exercise3/SuspendThreadDemo.java
+++ lab8/src/Exercise3/SuspendThreadDemo.java
@@ -1,5 +1,22 @@
 package Exercise3;
 
 public class SuspendThreadDemo {
+	public static void main(String[] args) {
 
+		//create object
+		Runnable selector1 = new SuspendThread();
+		Runnable selector2 = new SuspendThread();
+		Runnable selector3 = new SuspendThread();
+		
+		//create frame
+		Thread thread1 = new Thread(selector1,"text");
+		Thread thread2 = new Thread(selector2,"word1");
+		Thread thread3 = new Thread(selector3,"suspendText");
+	
+		//execute
+		thread3.start();
+		thread2.start();
+		thread1.start();
+		
+	}
 }
